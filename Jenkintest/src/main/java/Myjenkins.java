432d0be26950e0a654164871493f40f825a4437e
Himanshu <Himanshu@LAPTOP-EKBHIUMA>
2021-02-17 15:06:56 +0530

    next commit to test automated build

--- Jenkintest/src/main/java/Myjenkins.java
+++ Jenkintest/src/main/java/Myjenkins.java
@@ -3,6 +3,7 @@ public class Myjenkins {
 
 	public static void main(String[] args) {
 		System.out.println("This project is to test jenkins");
+		System.out.println("Lets automate the bulid ");
 
 	}
 
